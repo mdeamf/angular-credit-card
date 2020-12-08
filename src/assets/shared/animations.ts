916361178b569568bc5b5554c0308428ee7a1f52
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2020-12-08 15:52:51 -0300

    feat(assets): added animations Created a single file to gather all the shared animations that can be used by the application.

---
+++ src/assets/shared/animations.ts
@@ -0,0 +1,20 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
+export const fadeInAnimation = trigger('fadeInAnimation', [
+  state('in', style({ opacity: 1 })),
+  transition(':enter', [style({ opacity: 0 }), animate(350)]),
+  transition(':leave', animate(350, style({ opacity: 0 }))),
+]);
+
+export const routeStateTrigger = trigger('routeState', [
+  transition('*<=>*', [
+    style({ opacity: 0 }),
+    animate('0.5s', style({ opacity: 1 })),
+  ]),
+]);
