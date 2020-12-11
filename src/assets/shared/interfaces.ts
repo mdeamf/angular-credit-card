2544b10f04ac3010f967f6cc24f755577196c0ef
Mauricio Francisco <mauriciodealmeidamf@gmail.com>
2020-12-11 16:32:53 -0300

    feat(app): created shared interfaces file Added the shared Interfaces file to aggregate simple and global interfaces that can be used by the application.
    Currently, the only interface added was the Cards object.

---
+++ src/assets/shared/interfaces.ts
@@ -0,0 +1,11 @@
+export interface Cards {
+  id: Number;
+  number: String;
+  embossedName: String;
+  validThru: String;
+  securityCode: String;
+  brand: String;
+  isCreditActive: Boolean;
+  isDebitActive: Boolean;
+  isBlocked: Boolean;
+}
